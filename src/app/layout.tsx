434b91902a1ad67eec45daa62236f4e9577a3ec1
P4vanzinho <thiago772000@gmail.com>
2025-02-12 11:59:07 -0300

    refactor : change metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "School Management",
+  description: "Next.js School Management ",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
