633053e423eb2c2829fe32f45f4a24ad06070688
P4vanzinho <thiago772000@gmail.com>
2025-02-25 12:00:01 -0300

    feat : use created components

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,4 +1,8 @@
+import Announcements from "@/app/components/Announcements";
+import AttendenceChart from "@/app/components/AttendenceChart";
 import CountChart from "@/app/components/CountChart";
+import EventCalendar from "@/app/components/EventCalendar";
+import FinanceChart from "@/app/components/FinanceChart";
 import UserCard from "@/app/components/UserCard";
 
 const AdminPage = () => {
@@ -11,15 +15,23 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
-        {/*MIDLECARD*/}
+
         <div className="flex gap-4 flex-col lg:flex-row">
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
           </div>
-          <div className="w-full lg:w-2/3 h-[450px]"></div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendenceChart />
+          </div>
+        </div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
